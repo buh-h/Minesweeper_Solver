f448e0453c182bb9f0117e3c5ba91304dfbbdcf7
buh-h <159833212+buh-h@users.noreply.github.com>
2024-02-14 04:35:14 -0600

    Add files via upload
    Added click, chord, flag, identify, updateBoard methods

--- Solver.java
+++ Solver.java
@@ -15,6 +15,7 @@ public class Solver {
     // Currently set for easy (8x8)
     static int boardWidth = 8;
     static int boardHeight = 8;
+    static Robot robot; //Robot for mouse presses
 
     // 2D array to track the contents of the board
     // 1-8 represents numbered tiles
@@ -23,19 +24,21 @@ public class Solver {
     // -2 represents a mine
     static int[][] board = new int[boardWidth][boardHeight]; 
 
-    static void init() {
+    static void init() throws Throwable{
         System.out.println("Initializing Solver");
+        robot = new Robot();
         findFirstTile(); 
         // Initializes board with its default values
         board = new int[boardWidth][boardHeight]; 
         for (int i=0; i<boardWidth; i++) 
             for (int j=0; j<boardWidth; j++) 
                 board[i][j] = -1;
+        click(0, 0);
+        updateBoard();
     }
     // Takes a screenshot of the current screen
     static BufferedImage screenshot(){
         try {
-            Robot robot = new Robot();
             Rectangle captureSize = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
             ScreenWidth = captureSize.width;
             ScreenHeight = captureSize.height;
@@ -57,6 +60,8 @@ public class Solver {
         return Arrays.equals(rgb, new int[] {192, 192, 192});
     }
 
+
+
     // Finds the length of a tile for ease of programming (not used in program)
     static int tileLength()
     {
@@ -69,6 +74,14 @@ public class Solver {
                 return i;
         }
         return -1;
+    }
+    // Prints out board for debugging
+    static void printBoard() {
+        for (int i=0; i<boardWidth; i++) {
+            for (int j=0; j<boardHeight; j++) 
+                System.out.print(board[i][j] + "\t");
+            System.out.println("");
+        }
     }
     
     // Finds the coordinates of the top-leftmost tile 
@@ -100,20 +113,132 @@ public class Solver {
         }
     }
 
+    // Clicks specified tile
+    static void click(int i, int j) throws Throwable {
+        // Converts i and j to a pizel point
+        int x = firstTileX + i * 16; 
+        int y = firstTileY + j * 16;
+        robot.mouseMove(x, y);
+    
+        robot.mousePress(1024); // Left click
+        Thread.sleep(5); // wait
+        robot.mouseRelease(1024);
+        Thread.sleep(10);
+    }
+
+    // Chords specified tile
+    static void chord(int i, int j) throws Throwable {
+        // Converts i and j to a pizel point
+        int x = firstTileX + i * 16; 
+        int y = firstTileY + j * 16;
+        robot.mouseMove(x, y);
+    
+        robot.mousePress(1024); // Left click
+        robot.mousePress(4096); // Right click
+        Thread.sleep(5); // wait
+        robot.mouseRelease(1024);
+        robot.mouseRelease(4096);
+        Thread.sleep(10);
+    }
+
+    // Flags specified tile
+    static void flag(int i, int j) throws Throwable {
+        // Converts i and j to a pizel point
+        int x = firstTileX + i * 16; 
+        int y = firstTileY + j * 16;
+        robot.mouseMove(x, y);
+    
+        robot.mousePress(4096); // Right click
+        Thread.sleep(5); // wait
+        robot.mouseRelease(4096);
+        Thread.sleep(10);
+
+        board[i][j] = -2; // Updates the stored board with the flagged mine
+        // Calls chord on all 8 surrounding tiles 
+        // MAKE SURE THIS DOESN'T CAUSE ANY ISSUES LIKE UNFLAGGING A MINE
+        chord(i+1, j);
+        chord(i+1, j+1);
+        chord(i+1, j-1);
+        chord(i, j+1);
+        chord(i, j-1);
+        chord(i-1, j);
+        chord(i-1, j+1);
+        chord(i+1, j-1);
+    }
+    // Tests whether a tile has been cleared or not
+    static boolean isCleared(int i, int j, BufferedImage image) {
+        // Converts i and j to a pixel point
+        int x = firstTileX + i * 16; 
+        int y = firstTileY + j * 16;
+        // int k = 0;
+        // while (matchesTile(image.getRGB(x-k, y))) // Iterates backwards to find border of tile
+        //     k++;
+        
+        if ((image.getRGB(x-6, y) & 0xff) > 192) // if the left border is white, tile is uncleared
+            return false;
+        return true; // If left border is not white, tile is clearned
+    }
     // Identifies the current tile
+    // 1-8 represents numbered tiles
+    // 0 represented an empty tile
+    // -1 represents an unopened tile 
+    // -2 represents a mine
+    static int identify(int i, int j, BufferedImage image) {
+        if (board[i][j] == -2) {return -2;} // If tile is flagged, ignore it
+        // Converts i and j to a pixel point
+        int x = firstTileX + i * 16; 
+        int y = firstTileY + j * 16;
 
+        // takes a 4x4 area of pixels from the center of the tile
+        int pixels[] = new int[16];
+        int count = 0;
+        for(int n=x; n<x+4; n++)
+            for(int m=y; m<y+4; m++) {
+                pixels[count] = image.getRGB(n,m);
+                count++;
+            }   
+
+        for(int color: pixels) {
+
+            int red = (color & 0xff0000) >> 16;
+            int green = (color & 0xff00) >> 8;
+            int blue = color & 0xff;
+
+            if (!matchesTile(color)) { // Ignores the background tile color
+
+                if (blue >= 200) {return 1;}
+                if (red >= 200) {return 3;}
+                if (red >= 100 && green >= 100 && blue >= 100) {return 8;}
+                if (green >= 100 && blue >=100) {return 6;}
+                if (green >=100) {return 2;}
+                if (blue >= 100) {return 4;}
+                if (red >= 100) {return 5;};
+                if (red <= 100 && green <= 100 && blue <= 100) {return 7;}
+            }
+        }
+        // Need to differentiate between opened and unopened tile
+        if (isCleared(i, j, image)) {return 0;} 
+        return -1;
+    }
+
+    // Updates the content of board to match the game
+    static void updateBoard() {
+        BufferedImage image = screenshot();
+        for (int i=0; i<boardWidth; i++) 
+            for (int j=0; j<boardWidth; j++) 
+                board[j][i] = identify(i, j, image);
+    }
     // Main method
-    public static void main(String args[]) {  
+    public static void main(String args[]) throws Throwable {  
         init(); // Initializes solver
-        System.out.println(tileLength());
+        //printBoard();
 
 
 
 
 
 
-
-        // For testing colors
+        // //For testing colors
         // BufferedImage image = screenshot();
         // Point p;
         // while (true) {
